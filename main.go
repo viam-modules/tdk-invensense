d3f671646c4143d8a7c54a789cb8bfae21ed87e2
martha-johnston <martha.johnston@viam.com>
2024-12-06 15:07:29 -0500

    add mpu9250 model, no magnetometer

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"context"
 
 	"tdk-invensense/mpu6050"
+	"tdk-invensense/mpu9250"
 
 	"go.viam.com/rdk/components/movementsensor"
 	"go.viam.com/rdk/logging"
@@ -23,6 +24,10 @@ func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) err
 	}
 
 	if err = module.AddModelFromRegistry(ctx, movementsensor.API, mpu6050.Model); err != nil {
+		return err
+	}
+
+	if err = module.AddModelFromRegistry(ctx, movementsensor.API, mpu9250.Model); err != nil {
 		return err
 	}
 
