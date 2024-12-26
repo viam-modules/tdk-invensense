83532001b17a2c09223758c73f1fc8d4f2270149
martha-johnston <106617924+martha-johnston@users.noreply.github.com>
2024-12-26 16:08:34 -0500

    Use github repo address instead of local go module (#5)

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module tdk-invensense
+module github.com/viam-modules/tdk-invensense
 
 go 1.23.0
 
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ package main
 import (
 	"context"
 
-	"tdk-invensense/mpu6050"
+	"github.com/viam-modules/tdk-invensense/mpu6050"
 
 	"go.viam.com/rdk/components/movementsensor"
 	"go.viam.com/rdk/logging"
